0bf1e3fd9f7b88c46a677e06ce4ff5e1cac968a8
Fares Fraij <fares@utexas.edu>
2020-06-09 19:48:31 -0500

    uploading additional material

--- exercises/IsPrime2T.py
+++ exercises/IsPrime2T.py
@@ -1,36 +1,36 @@
 #!/usr/bin/env python3
 
 # ------------
-# IsPrime1T.py
+# IsPrime2T.py
 # ------------
 
 # https://en.wikipedia.org/wiki/Primality_test
 
 from unittest import main, TestCase
 
-from IsPrime1 import is_prime
+from IsPrime2 import is_prime
 
 class MyUnitTests (TestCase) :
     def test_01 (self) :
-        self.assertFalse(is_prime(1))
+        self.assertFalse(is_prime( 1))
 
     def test_02 (self) :
-        self.assertFalse(is_prime(2))
+        self.assertTrue(is_prime( 2))
 
     def test_03 (self) :
-        self.assertTrue(is_prime(3))
+        self.assertTrue(is_prime( 3))
 
     def test_04 (self) :
-        self.assertFalse(is_prime(4))
+        self.assertFalse(is_prime( 4))
 
     def test_05 (self) :
-        self.assertTrue(is_prime(5))
+        self.assertTrue(is_prime( 5))
 
     def test_07 (self) :
-        self.assertTrue(is_prime(7))
+        self.assertTrue(is_prime( 7))
 
     def test_09 (self) :
-        self.assertTrue(is_prime(9))
+        self.assertFalse(is_prime( 9))
 
     def test_27 (self) :
         self.assertFalse(is_prime(27))
@@ -42,7 +42,7 @@ if __name__ == "__main__" :
     main()
 
 """ #pragma: no cover
-% IsPrime1T.py
+$ IsPrime2T.py
 .........
 ----------------------------------------------------------------------
 Ran 9 tests in 0.001s
